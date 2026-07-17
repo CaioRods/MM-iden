f8b241fe39bf805391634b9aafc8b586f5ea6265
Caio Rodrigues <caiorodrigues@MacBook-Pro-de-Caio.local>
2026-07-17 09:50:34 -0300

    Configure M&M Cebolas details: name, logo, products, and color scheme

---
+++ data.js
@@ -0,0 +1,9 @@
+const initialData = {
+  "products": [
+    { codigo: '101001', produto: 'Cebola (caixa1)', quantidade: ' ' },
+    { codigo: '101002', produto: 'Cebola (caixa2)', quantidade: ' ' },
+    { codigo: '101003', produto: 'Cebola (caixa3)', quantidade: ' ' },
+    { codigo: '101004', produto: 'Cebola Roxa', quantidade: ' ' }
+  ],
+  "highlights": []
+};
