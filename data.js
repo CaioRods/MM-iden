d8ff78aae32d8d00db488caa30590e622c341b54
Caio Rodrigues <caiorodrigues@MacBook-Pro-de-Caio.local>
2026-07-17 10:01:12 -0300

    Update logo in 3D preview sheet, remove QR code from back, increase logo size by 30%, and style print button to orange-yellow

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 const initialData = {
   "products": [
-    { codigo: '101001', produto: 'Cebola (caixa1)', quantidade: ' ' },
-    { codigo: '101002', produto: 'Cebola (caixa2)', quantidade: ' ' },
-    { codigo: '101003', produto: 'Cebola (caixa3)', quantidade: ' ' },
+    { codigo: '101001', produto: 'Cebola (CX-1)', quantidade: ' ' },
+    { codigo: '101002', produto: 'Cebola (CX-2)', quantidade: ' ' },
+    { codigo: '101003', produto: 'Cebola (CX-3)', quantidade: ' ' },
     { codigo: '101004', produto: 'Cebola Roxa', quantidade: ' ' }
   ],
   "highlights": []
